fb567599dc4fe4e153f00d33cc5d041b9af3756f
simon louvet <simon.louvet.pro@gmail.com>
2022-12-03 02:22:11 +0100

    deataserver

---
+++ addOn/frontend/src/config/dataServers.js
@@ -0,0 +1,10 @@
+const dataServers = {
+  av: {
+    baseUrl: process.env.REACT_APP_MIDDLEWARE_URL,
+    authServer: true,
+    default: true,
+    uploadsContainer: '/files'
+  }
+};
+
+export default dataServers;
