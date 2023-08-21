29a892f9739588dd0d8c10f3c5d8d16a6bdbf570
mguihal <m.guihal@gmail.com>
2023-08-21 22:39:20 +0200

    fix: Organization members edition

---
+++ addOn/frontend/src/resources/Agent/Actor/Organization/OrganizationEdit.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  TextInput,
+  SelectInput,
+  TabbedForm,
+  ImageField,
+  AutocompleteInput,
+  ArrayInput,
+  SimpleFormIterator,
+} from 'react-admin';
+import makeStyles from '@mui/styles/makeStyles';
+import { ReferenceInput, ImageInput } from '@semapps/input-components';
+import { MarkdownInput } from '@semapps/markdown-components';
+import { MultiLinesInput } from '@semapps/input-components';
+// import { ReificationArrayInput } from "@semapps/semantic-data-provider";
+import { OrganizationsInput, EventsInput, ThemesInput, DocumentsInput, LocationInput } from '../../../../common/input';
+import Title from "../../../../layout/Title";
+import Edit from "../../../../layout/edit/Edit";
+
+const useHideInputStyles = makeStyles({
+  root: {
+    display: 'none'
+  }
+});
+
+const ReificationArrayInput = props => {
+  const { reificationClass, children, ...otherProps } = props;
+  const hideInputStyles = useHideInputStyles();
+
+  return (
+    <ArrayInput {...otherProps}>
+      <SimpleFormIterator inline>
+        {props.children}
+        <TextInput className={hideInputStyles.root} source="type" defaultValue={reificationClass} />
+      </SimpleFormIterator>
+    </ArrayInput>
+  );
+};
+
+export const OrganizationEdit = props => (
+  <Edit title={<Title />} redirect="show" {...props}>
+    <TabbedForm>
+      <TabbedForm.Tab label="Données">
+        <TextInput source="pair:label" fullWidth />
+        <TextInput source="pair:comment" fullWidth />
+        <MarkdownInput source="pair:description" fullWidth />
+        <ReferenceInput reference="Status" source="pair:hasStatus" filter={{ a: 'pair:AgentStatus' }}>
+          <SelectInput optionText="pair:label" />
+        </ReferenceInput>
+        <ReferenceInput reference="Type" source="pair:hasType" filter={{ a: 'pair:OrganizationType' }}>
+          <SelectInput optionText="pair:label" />
+        </ReferenceInput>
+        <MultiLinesInput source="pair:homePage" fullWidth />
+        <TextInput source="pair:e-mail" fullWidth type="email" />
+        <LocationInput source="pair:hasLocation" fullWidth />
+        <ImageInput source="image" accept="image/*">
+          <ImageField source="src" />
+        </ImageInput>
+      </TabbedForm.Tab>
+      <TabbedForm.Tab label="Membres">
+        <ReificationArrayInput source="pair:organizationOfMembership" reificationClass="pair:MembershipAssociation">
+          <ReferenceInput reference="Person" source="pair:membershipActor">
+            <AutocompleteInput
+              optionText={record => record && `${record['pair:firstName']} ${record['pair:lastName']}`}
+              shouldRenderSuggestions={value => value && value.length > 1}
+              label="Membre"
+              size="small"
+              sx={{
+                mt: 1,
+                mb: '4px',
+                minWidth: 300,
+              }}
+            />
+          </ReferenceInput>
+          <ReferenceInput reference="MembershipRole" source="pair:membershipRole">
+            <SelectInput optionText="pair:label" label="Rôle" />
+          </ReferenceInput>
+        </ReificationArrayInput>
+      </TabbedForm.Tab>
+      <TabbedForm.Tab label="Relations">
+        <OrganizationsInput source="pair:partnerOf" />
+        <EventsInput source="pair:involvedIn" />
+        <ThemesInput source="pair:hasTopic" />
+        <DocumentsInput source="pair:documentedBy" />
+      </TabbedForm.Tab>
+    </TabbedForm>
+  </Edit>
+);
+
+export default OrganizationEdit;
---
+++ addOn/frontend/src/resources/Agent/Actor/Organization/index.js
@@ -0,0 +1,53 @@
+import CreateOrImport from '../../../../common/CreateOrImport';
+import OrganizationEdit from './OrganizationEdit';
+import OrganizationList from './OrganizationList';
+import OrganizationShow from './OrganizationShow';
+import HomeIcon from '@mui/icons-material/Home';
+
+export default {
+  config: {
+    list: OrganizationList,
+    show: OrganizationShow,
+    create: CreateOrImport,
+    edit: OrganizationEdit,
+    icon: HomeIcon,
+    options: {
+      label: 'Organisations',
+      parent: 'Actor'
+    }
+  },
+  dataModel: {
+    types: ['pair:Organization'],
+    list: {
+      servers: '@default',
+      forceArray: ['pair:organizationOfMembership']
+    },
+    fieldsMapping: {
+      title: 'pair:label'
+    }
+  },
+  translations: {
+    fr: {
+      name: 'Organisation |||| Organisations',
+      fields: {
+        'pair:label': 'Nom',
+        'pair:comment': 'Courte description',
+        'pair:description': 'Description',
+        'pair:homePage': 'Site web',
+        'pair:e-mail': 'E-mail de contact',
+        image: 'Logo',
+        'pair:affiliates': 'A pour membres',
+        'pair:partnerOf': 'Partenaire de',
+        'pair:involvedIn': 'Impliqué dans',
+        'pair:hasType': 'Type',
+        'pair:hasStatus': 'Statut',
+        'pair:hasTopic': 'A pour thème',
+        'pair:documentedBy': 'Documenté par',
+        'pair:hasLocation': 'Adresse',
+        'pair:organizationOfMembership': 'Membres avec Rôle',
+        'pair:membershipActor': 'Membre',
+        'pair:membershipRole': 'Rôle'
+      }
+    }
+  }
+};
