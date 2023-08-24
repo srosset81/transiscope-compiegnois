5c94da6333aec3bd892f29ffd5776fcde59e14a7
simon louvet <simon.louvet.pro@gmail.com>
2023-08-24 03:02:50 +0200

    add image to event

---
+++ addOn/frontend/src/resources/Agent/Activity/Event/EventShow.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { TextField, UrlField, DateField } from 'react-admin';
+import { Grid } from '@mui/material';
+import { AvatarWithLabelField, QuickAppendReferenceArrayField, ReferenceArrayField } from '@semapps/field-components';
+import { GridList, ChipList } from '@semapps/list-components';
+import { MapField } from '@semapps/geo-components';
+import { MarkdownField } from '../../../../common/field';
+import { Hero, MainList, SideList } from '../../../../common/list';
+import Title from "../../../../layout/Title";
+import Show from "../../../../layout/show/Show";
+
+const EventShow = props => (
+  <Show title={<Title />} {...props}>
+    <Grid container spacing={5}>
+      <Grid item xs={12} sm={9}>
+        <Hero image="image">
+          <TextField source="pair:comment" />
+          <DateField source="pair:startDate" showTime />
+          <DateField source="pair:endDate" showTime />
+          <UrlField source="pair:aboutPage" />
+          <MapField
+            source="pair:hasLocation"
+            address={record => record['pair:hasLocation'] && record['pair:hasLocation']['pair:label']}
+            latitude={record => record['pair:hasLocation'] && record['pair:hasLocation']['pair:latitude']}
+            longitude={record => record['pair:hasLocation'] && record['pair:hasLocation']['pair:longitude']}
+          />
+        </Hero>
+        <MainList>
+          <MarkdownField source="pair:description" />
+        </MainList>
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <SideList>
+          <ReferenceArrayField reference="Actor" source="pair:involves" sort={{ field: 'type', order: 'ASC' }}>
+            <GridList xs={6} linkType="show" externalLinks>
+              <AvatarWithLabelField label="pair:label" image="image" />
+            </GridList>
+          </ReferenceArrayField>
+          <QuickAppendReferenceArrayField reference="Theme" source="pair:hasTopic">
+            <ChipList primaryText="pair:label" linkType="show" externalLinks />
+          </QuickAppendReferenceArrayField>
+        </SideList>
+      </Grid>
+    </Grid>
+  </Show>
+);
+
+export default EventShow;
