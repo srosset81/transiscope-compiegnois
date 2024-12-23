13b30d703ecee108bc43e1e80e22273263c48407
mguihal <m.guihal@gmail.com>
2024-12-23 01:37:12 +0100

    Upgrade to Archipelago v2.4.1

--- addOn/frontend/src/customResources.js
+++ addOn/frontend/src/customResources.js
@@ -7,13 +7,6 @@ delete customizedResources['Group'];
 delete customizedResources['Task'];
 delete customizedResources['Idea'];
 
-// Change list fetching method to container
-Object.keys(customizedResources).forEach((key) => {
-  if (customizedResources[key].dataModel?.list) {
-    customizedResources[key].dataModel.list.fetchContainer = true;
-  }
-});
-
 // Change "Organisation" to "Alternative" wording
 customizedResources['Organization'].translations.fr.name = 'Alternative |||| Alternatives';
 customizedResources['Organization'].translations.fr.searchLabel = 'Rechercher une alternative';
